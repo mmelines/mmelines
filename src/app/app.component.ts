845faeaf38884004290cb383abed93cf25a0c52f
mmelines <mmebit@icloud.com>
2022-05-15 20:20:03 -0400

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, VERSION } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'resume',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ResumeComponent } from './resume/resume.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     RouterModule.forRoot([{ path: '', component: ResumeComponent }]),
   ],
-  declarations: [AppComponent, ResumeComponent, TopBarComponent],
+  declarations: [
+    AppComponent,
+    ResumeComponent,
+    TopBarComponent,
+    NavBarComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
